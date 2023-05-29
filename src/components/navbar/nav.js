7bdaf5a249239b60b2753739cfb2b6e6901d759a
Lukblan <luk.blanko@gmail.com>
2023-05-28 21:12:45 -0300

    Change navbar styles

--- src/components/navbar/nav.js
+++ src/components/navbar/nav.js
@@ -4,6 +4,7 @@ import '../../styles.css';
 function generateNavBar() {
   const nav = document.createElement("nav");
   const header = document.createElement("h1");
+  header.textContent = "Restaurant Name"
   const tabList = generateTabList();
   nav.append(header);
   nav.append(tabList);
@@ -11,12 +12,18 @@ function generateNavBar() {
 }
 
 function generateTabList() {
-  const list = document.createElement("li");
+  const tabList = document.createElement("ul");
   const homeTab = document.createElement("li");
   const menuTab = document.createElement("li");
   const contactTab = document.createElement("li");
-  list.append(homeTab);
-  list.append(menuTab);
-  list.append(contactTab);
-  return list;
+
+  homeTab.textContent = "Home";
+  menuTab.textContent = "Menu";
+  contactTab.textContent = "Contact";
+
+  tabList.classList.add("tab-menu");
+  tabList.append(homeTab);
+  tabList.append(menuTab);
+  tabList.append(contactTab);
+  return tabList;
 }
