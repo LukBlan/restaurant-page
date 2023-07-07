fd0b5dc3c9ba599753b58d8c6715703ce1241cfd
Lukblan <luk.blanko@gmail.com>
2023-07-07 20:49:46 -0300

    Add new info section on food image

--- src/components/menu-section/menu-section.js
+++ src/components/menu-section/menu-section.js
@@ -6,10 +6,22 @@ import Steak from "./images/steak.jpg"
 export {generateMenuSection}
 
 const foodImages = {
-  "hamburger": Hamburger,
-  "pizza": Pizza,
-  "ravioli": Ravioli,
-  "steak": Steak,
+  "hamburger": {
+    name: "Deluxe Burger",
+    src: Hamburger,
+  },
+  "pizza": {
+    name: "Ultramarine Pizza",
+    src: Pizza,
+  },
+  "ravioli": {
+    name: "Green day Ravioli",
+    src: Ravioli,
+  },
+  "steak": {
+    name: "Classic Steak",
+    src: Steak,
+  },
 }
 
 function generateMenuSection() {
@@ -27,9 +39,36 @@ function generateMenuSection() {
 }
 
 function generateFoodBox(foodName) {
+  const container = document.createElement("div");
+  const foodObject = foodImages[foodName]
+  const foodImage = getFoodImage(foodObject);
+  const foodInfo = getFoodInfo(foodObject);
+
+  container.classList.add("food-card");
+  container.append(foodImage);
+  container.append(foodInfo);
+  return container;
+}
+
+function getFoodImage(foodObject) {
   const foodImage = new Image();
   console.log(Hamburger.src)
-  foodImage.src = foodImages[foodName];
-  foodImage.classList.add("menu-image");
+  foodImage.src = foodObject.src;
+  foodImage.classList.add("food-image");
   return foodImage;
+}
+
+function getFoodInfo(foodObject) {
+  const container = document.createElement("div");
+  const foodNameBox = generateFoodNameContainer(foodObject.name);
+
+  container.classList.add("food-info");
+  container.append(foodNameBox);
+  return container;
+}
+
+function generateFoodNameContainer(foodName) {
+  const foodNameBox = document.createElement("h3");
+  foodNameBox.innerText = foodName;
+  return foodNameBox;
 }
