cbeeef0dc9e22e1ce633d10ca1eb639f421a3cd3
Lukblan <luk.blanko@gmail.com>
2023-07-06 21:21:37 -0300

    Add new home menu with images

--- src/components/menu-section/menu-section.js
+++ src/components/menu-section/menu-section.js
@@ -1,8 +1,46 @@
-import {generateSection} from "../tabs-functions/tabs-functions.js";
+import Hamburger from "./images/hamburger.jpg"
+import Pizza from "./images/pizza.jpg"
+import Ravioli from "./images/ravioli.jpg"
+import Steak from "./images/steak.jpg"
 
 export {generateMenuSection}
 
+const foodImages = {
+  "hamburger": Hamburger.src,
+  "pizza": Pizza.src,
+  "ravioli": Ravioli.src,
+  "steak": Steak.src,
+}
+
+
 function generateMenuSection() {
-  return generateSection("Menu");
+  const foodList = ["hamburger", "pizza", "ravioli", "steak"];
+  const container = generateSection("Menu");
+  foodList.forEach(food => {
+    const foodBox = generateFoodBox(food);
+    container.append(foodBox);
+  })
+  return container;
+}
+
+function generateFoodBox(foodName) {
+  const foodImage = new Image();
+  foodImage.src = foodImages[foodName];
+  foodImage.classList.add("menu-image");
+  return foodImage;
+}
+
+function generateSection(name) {
+  const container = document.createElement("div");
+  const title = generateTitleElement(name);
+  container.classList.add("tab-section");
+  container.append(title);
+  return container;
+}
+
+function generateTitleElement(titleName) {
+  const title = document.createElement("h2");
+  title.innerText = titleName
+  return title;
 }
 
